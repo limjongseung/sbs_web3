ecc7641de8e6d4d3081c09c3d9fe52a3603d7b3a
jongseungim <jongseungim5348@gmail.com>
2022-07-04 19:33:46 +0900

    gnb

---
+++ 웹3 - 9day/js/gnb.js
@@ -0,0 +1,45 @@
+$(function(){
+
+
+
+
+    $(".nav a").on("mouseover",function(){
+
+        let position = $(this).parent().position();
+        let width  = $(this).parent().width();
+
+        $(".nav .slide2").css({
+            left: +position.left,
+            width: width,
+            opacity : 1
+        });
+
+    }) ;
+
+    $(".nav a").on("mouseout",function(){
+        $(".nav .slide2").css({opacity : 0})
+    });
+
+    $(".nav a").on("click",function(){
+        let position = $(this).parent().position();
+        let width  = $(this).parent().width();
+
+        $(".nav .slide1").css({
+            left : +position.left,
+            width : width
+        });
+
+    });
+
+    let position = $(".nav li:nth-of-type(1) a").parent().position()    ;
+    let width = $(".nav li:nth-of-type(1) a").parent().width();
+
+    $(".nav .slide1").css({
+        left: +position.left,
+        width : width,
+        opacity : 1
+    });
+
+
+
+});

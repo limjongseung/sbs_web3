5afda18a85ccd1908cda653d93fe73390e116107
jongseungim <jongseungim5348@gmail.com>
2022-07-06 19:50:29 +0900

    slide 수업 (slick js)

---
+++ 웹3 - 10day/슬라이드/js/slide.js
@@ -0,0 +1,60 @@
+
+
+$(".slider").slick({
+    infinite:true, // 무한반복 옵션  true / false
+    speed : 1000,  // 다음 슬라이드가 나오는 데까지 걸리는 시간
+    autoplay : false, //자동재생
+    pauseOnHover : true,// 슬라이드에 마우스 호버하면 자동재생 멈춤 설정
+    autoplaySpeed : 3000, // 자동재생 속동 
+    vertical : false, // 세로방향 슬라이드 옵션
+    draggable : false, //드래그 슬라이드 옵션
+    dots : true, //슬라이드페이지네이션 옵션
+    arrows:true, //previous & next 버튼 옵션
+    slidesToShow: 2,//한 화면에 보여질 슬라이드 갯수
+    slidesToScroll: 2, //한번에 슬라이드가 되는 콘텐츠 개수
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

14bed3edc0045145c1607b432f192decb573ce51
lim5348 <jongseungim5348@gmail.com>
2022-07-07 11:31:41 +0900

    accordion

---
+++ Day09/js/accordion2.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+    $("dt").click(function(){
+        $(this).toggleClass("open");
+        if($(this).hasClass("open")){$("dt").not(this).removeClass("open")};
+    });
+});
+
+
+
+
+
+
+
+

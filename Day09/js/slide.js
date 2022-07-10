2620f7ae363723dd893d328cf1e57e791e3f3c2c
lim5348 <jongseungim5348@gmail.com>
2022-07-10 13:40:13 +0900

    slide복습

---
+++ Day09/js/slide.js
@@ -0,0 +1,37 @@
+
+
+
+$(function(){
+
+let i = 0; //슬라이드 넘버1
+$(".next").click(function(){
+    i++; // 슬라이드 넘버 + 1증가
+    if(i>3){ // i가 3보다 크다면
+        i=3; //구동하지 않음
+    }; //작다면 아래 구문 실행
+    $(".slide_container .slide").animate({marginLeft:-1200*i+"px"});
+});
+
+$(".prev").click(function(){
+    i--;
+    if(i<0){
+        i=0;
+    };
+    $(".slide_container .slide").animate({marginLeft:-1200*i +"px"});
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
